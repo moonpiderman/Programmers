c8e7bb1255cdcaf04e65e6a90293a32a2eb4d5d9
moonpiderman <lotsofchuck@gmail.com>
2021-04-26 15:57:06 +0900

    re

--- 12903/12903.py
+++ 12903/12903.py
@@ -1,10 +1,10 @@
 def solution(s):
     str_len = len(s)
-    
-    if int(str_len % 2) == 1 :
-        answer = s[str_len//2]  
-    
-    elif int(str_len % 1) == 0 :
+
+    if int(str_len % 2) == 1:
+        answer = s[str_len//2]
+
+    elif int(str_len % 2) == 0:
         answer = s[(str_len//2) - 1] + s[str_len//2]
-    
+
     return answer
--- 12903/README.md
+++ 12903/README.md
@@ -3,3 +3,10 @@
 ## Quiz
 
 ![initial](https://user-images.githubusercontent.com/70942197/116040326-d861c480-a6a6-11eb-8ec1-7e1aebf5509c.png)
+
+## Solution
+
+1. len() 함수를 사용해서 'str_len' 변수에 넣어준다.
+2. 'str_len'를 활용해 짝수와 홀수일 경우를 나눈다.
+   홀수일 경우에는 가운데의 문자를 바로 출력해주고
+   짝수라면 반으로 나눈 지점의 문자와 바로 앞의 문자를 출력해준다.
