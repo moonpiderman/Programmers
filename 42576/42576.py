0343050365dc114436e0a0dc6e63ef80a189afda
moonpiderman <lotsofchuck@gmail.com>
2021-04-26 22:37:22 +0900

    sol

--- 42576/42576.py
+++ 42576/42576.py
@@ -1,9 +1,9 @@
 def solution(participant, completion):
     dic = {}
-    for x in participant:
-        dic[x] = dic.get(x, 0) + 1
-    for x in completion:
-        dic[x] -= 1
+    for value in participant:
+        dic[value] = dic.get(value, 0) + 1
+    for value in completion:
+        dic[value] -= 1
 
     result = [i for i, j in dic.items() if j > 0]
     answer = result[0]
--- 42576/README.md
+++ 42576/README.md
@@ -2,6 +2,11 @@
 
 ## Quiz
 
-![initial]
+![initial](https://user-images.githubusercontent.com/70942197/116091296-a456c580-a6df-11eb-8148-5fe058d73121.png)
 
 ## Solution
+
+1. 해시 알고리즘에 대한 이해가 필요하다.
+2. get 함수를 이용해 해당 value값이 존재하면 1을 넣어주고 같은 값이 등장하면 +1
+3. completion 리스트에 같은 값이 있다면 -1을 해준다.
+4. items 함수를 활용해 key와 value를 얻어낸다.
