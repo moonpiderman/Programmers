6fe1b7de5617d44f2631186821e069b8d5ae0af0
moonpiderman <lotsofchuck@gmail.com>
2021-04-29 11:42:54 +0900

    test readme

--- Stack_Queue/42583/42583.py
+++ Stack_Queue/42583/42583.py
@@ -1,1 +1,16 @@
-from queue import Queue
+def solution(bridge_length, weight, truck_weights):
+    count_time = 0
+    queue = [0 for i in range(bridge_length)]
+
+    while queue:
+        # 시간은 어차피 흘러야 함
+        count_time += 1
+        # 앞에 놈을 끄집어 낸다.
+        queue.pop(0)
+        if truck_weights:
+            if sum(queue) + truck_weights[0] <= weight:
+                queue.append(truck_weights.pop(0))
+            else:
+                queue.append(0)
+
+    return count_time
