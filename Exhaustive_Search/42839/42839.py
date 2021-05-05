a76071eeee7aac109b23bcd724d4ef04c9e106a4
moonpiderman <lotsofchuck@gmail.com>
2021-05-05 14:19:49 +0900

    must edit it

--- Exhaustive_Search/42839/42839.py
+++ Exhaustive_Search/42839/42839.py
@@ -1,8 +1,27 @@
 from itertools import permutations
 
 
+def is_prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+        else:
+            return True
+
+
 def solution(numbers):
+    sep = [r for r in numbers]
+    total = []
     answer = 0
+
+    for i in range(1, len(sep) + 1):
+        total.append(list(map(''.join, permutations(sep, i))))
+
+    q = list(map(int, sum(total, [])))
+    q = list(set(q))
+    for i in q:
+        if is_prime(i) == True:
+            answer += 1
     return answer
 
 
@@ -10,6 +29,7 @@ def solution(numbers):
 numbers = "011"
 sep = [r for r in numbers]
 total = []
+result = []
 
 for i in range(1, len(sep) + 1):
     # 순열들을 total 리스트에 나열해주기
@@ -19,4 +39,7 @@ for i in range(1, len(sep) + 1):
 q = list(map(int, sum(total, [])))
 # 중복 제거
 q = list(set(q))
-print(q)
+
+for i in q:
+    if is_prime(i) == True:
+        result.append(i)
