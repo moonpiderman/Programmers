7544ce1ad711c5bbc1be06fac3b7bab26f2055d8
moonpiderman <lotsofchuck@gmail.com>
2021-05-06 13:46:06 +0900

    must-edit 42587

--- Stack_Queue/42587/42587.py
+++ Stack_Queue/42587/42587.py
@@ -1,25 +1,40 @@
 from collections import deque
 
-
 def solution(priorities, location):
-    priorities = deque(priorities)
-    queue = []
-
-    for i in range(1, len(priorities)):
-        if priorities[0] < priorities[i]:
-            temp = priorities.popleft()
-            queue.append(temp)
-            break
-        else:
-            continue
-
-    print(priorities)
-    return priorities
+    # location은 인덱스 값
+    # return은 실질적 n번째인지
+    answer = 0
+    dic = {i: priorities[i] for i in range(len(priorities))}
 
 
-# pri = [2, 1, 3, 2]
-# loca = 2
-pri = [1, 1, 9, 1, 1, 1]
-loca = 0
+    return answer
 
-solution(pri, loca)
+# dic = {i: priorities[i] for i in range(len(priorities))}
+
+priorities = [1, 1, 9, 1, 1, 1]
+location = 0
+
+pop_idx = 0
+pop_pri = 0
+result = []
+
+# 인덱스 deque 생성
+index = []
+for i in range(len(priorities)) :
+    index.append(i)
+index = deque(index)
+
+# deque of priorities
+p = deque(priorities)
+
+for i in range(len(priorities)) :
+    for j in range(len(priorities)) :
+        if priorities[i] < priorities[j] :
+            pop_idx = index.popleft()
+            pop_pri = p.popleft()
+            index.append(pop_idx)
+            p.append(pop_pri)
+        else :
+            result.append(p[i])
+
+print(result)
