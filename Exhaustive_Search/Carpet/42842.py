47bdf60dee7b648bbaffe4a41383f01aaf4f2fc4
moonpiderman <lotsofchuck@gmail.com>
2021-05-08 00:51:53 +0900

    edit

--- Exhaustive_Search/Carpet/42842.py
+++ Exhaustive_Search/Carpet/42842.py
@@ -28,31 +28,3 @@ def divisor(num) :
         if num % i == 0 :
             div.append([i, num//i])
     return div
-
-answer = []
-brown = 8
-yellow = 1
-
-y_chk = []
-
-for i in range(1, yellow+1) :
-    if yellow % i == 0 :
-        y_chk.append(i)
-
-y_list = divisor(yellow)
-b_list = [[i[0]+2, i[1]+2] for i in y_list]
-
-
-if len(y_chk) % 2 == 1 :
-    for i in b_list :
-        if i[0] == i[1] :
-            answer.append(i[0])
-            answer.append(i[1])
-elif len(y_chk) % 2 == 0 :
-    for i in b_list :
-        if i[0] > i[1] :
-            if (i[0] * i[1]) == (brown + yellow) :
-                answer.append(i[0])
-                answer.append(i[1])
-
-print(answer)
