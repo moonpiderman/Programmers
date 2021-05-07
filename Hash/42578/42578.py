10f783f185779b23fd6a89e342d5f10c5e79fc3f
moonpiderman <lotsofchuck@gmail.com>
2021-05-07 15:18:50 +0900

    fin 42578

--- Hash/42578/42578.py
+++ Hash/42578/42578.py
@@ -1,3 +1,37 @@
 def solution(clothes):
-    answer = 0
-    return answer
+    answer = 1
+    closet = {}
+
+    for clo in clothes :
+        type_cloth = clo[1]
+        name_cloth = clo[0]
+        if type_cloth in closet :
+            closet[type_cloth].append(name_cloth)
+        else :
+            closet[type_cloth] = [name_cloth]
+
+    for type_cloth in closet.keys() :
+        answer = answer * (len(closet[type_cloth]) + 1)
+    return answer - 1
+
+clothes = [
+    ["yellowhat", "headgear"],
+    ["bluesunglasses", "eyewear"],
+    ["green_turban", "headgear"]
+]
+
+answer = 1
+closet = {}
+for clo in clothes :
+    type_cloth = clo[1]
+    name_cloth = clo[0]
+    if type_cloth in closet :
+        closet[type_cloth].append(name_cloth)
+    else :
+        closet[type_cloth] = [name_cloth]
+
+for type_cloth in closet.keys() :
+    answer = answer * (len(closet[type_cloth]) + 1)
+
+answer -= 1
+print(answer)
