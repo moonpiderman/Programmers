e4aced62038e080b9dd506b3c4e49d9a4ca25afc
moonpiderman <lotsofchuck@gmail.com>
2021-05-08 20:02:37 +0900

    add main func

---
+++ DFS_BFS/Target_Number/43165.py
@@ -0,0 +1,12 @@
+def solution(numbers, target):
+    answer = 0
+    return answer
+
+
+
+
+if __name__ == '__main__' :
+    numbers = [1, 1, 1, 1, 1]
+    target = 3
+
+    print(solution(numbers, target))
