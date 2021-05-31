c0f5e3c8ef01d0ce3c1ae98638dac7668607a05d
moonpiderman <lotsofchuck@gmail.com>
2021-05-31 12:00:17 +0900

    joystick fin

--- Greedy/Joy_Stick/42860.py
+++ Greedy/Joy_Stick/42860.py
@@ -1,11 +1,40 @@
 def solution(name):
-    count = 0
-    name.upper()
+    cnt = 0
+    name = list(name.upper())
+    i = 0
 
-    return count
+    while True:
+        cnt += min(ord(name[i]) - ord('A'), ord('Z') - ord(name[i]) + 1)
+        name[i] = 'A'
+
+        if name.count('A') == len(name) :
+            return cnt
+
+        left, right = 1, 1
+        for l in range(1, len(name)) :
+            if name[i - l] == 'A':
+                left += 1
+            else:
+                break
+
+        for r in range(1, len(name)) :
+            if name[i + r] == 'A':
+                right += 1
+            else :
+                break
+
+        if left < right :
+            cnt += left
+            i -= left
+        else :
+            cnt += right
+            i += right
+    return cnt
 
 if __name__ == '__main__' :
     name_1 = "JEROEN"
     name_2 = "JAN"
+    name_3 = "jijo"
+    name_4 = "AZAAAZ"
 
-    print(solution(name_1))
+    print(solution(name_4))
