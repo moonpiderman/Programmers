ed1b95ec02cae1c59003a5b0eb5a2b202da92c25
moonpiderman <lotsofchuck@gmail.com>
2021-06-06 21:12:46 +0900

    again big number

---
+++ Greedy/Big_Number/42883.py
@@ -0,0 +1,21 @@
+from itertools import permutations
+
+def solution(number, k):
+    answer = ''
+    number = list(number)
+    permu = list(permutations(number, k))
+    print(permu)
+
+    return answer
+
+if __name__ == '__main__' :
+    number_1 = "1924"
+    k_1 = 2
+
+    number_2 = "1231234"
+    k_2 = 3
+
+    number_3 = "4177252841"
+    k_3 = 4
+
+    solution(number_1, k_1)
