b3c64133646c923231cd25d7a3414dd77ebbbc36
moonpiderman <lotsofchuck@gmail.com>
2021-06-18 11:06:01 +0900

    let's

--- Hash/Best_album/42579.py
+++ Hash/Best_album/42579.py
@@ -1,3 +1,11 @@
 def solution(genres, plays):
     answer = []
+
+
     return answer
+
+if __name__ == '__main__' :
+    genres_1 = ["classic", "pop", "classic", "classic", "pop"]
+    plays_1 = [500, 600, 150, 800, 2500]
+
+    print(solution(genres_1, plays_1))
