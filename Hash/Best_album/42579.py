acd0b52b89a6211bbd964f0110b14fada5ad8e3c
moonpiderman <lotsofchuck@gmail.com>
2021-06-18 13:06:03 +0900

    done

--- Hash/Best_album/42579.py
+++ Hash/Best_album/42579.py
@@ -1,6 +1,19 @@
 def solution(genres, plays):
     answer = []
+    total_plays = {}
+    numbering = {}
 
+    for i in range(len(genres)) :
+        genre = genres[i]
+        play = plays[i]
+        total_plays[genre] = total_plays.get(genre, 0) + play
+        numbering[genre] = numbering.get(genre, []) + [(play, i)]
+
+    genreSort = sorted(total_plays.items(), reverse=True)
+
+    for (genre, counts) in genreSort :
+        numbering[genre] = sorted(numbering[genre], key=lambda x: (-x[0], x[1]))
+        answer += [ i for play, i in numbering[genre][:2] ]
 
     return answer
 
