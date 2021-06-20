0dc04f5b889b1967a9070602c0cc1632b420780d
moonpiderman <lotsofchuck@gmail.com>
2021-06-20 20:05:32 +0900

    compression

---
+++ 2020_KAKAO_RECRUIT/String_compression/60057.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = 0
+    return answer
+
+if __name__ == '__main__' :
+    s_1 = "aabbaccc"
+    s_2 = "ababcdcdababcdcd"
+    s_3 = "abcabcdede"
+    s_4 = "abcabcabcabcdededededede"
+    s_5 = "xababcdcdababcdcd"
+
+    print(solution(s_1))
