bcceb4b054f335c0228f30d06a03724d83d13128
moonpiderman <lotsofchuck@gmail.com>
2021-06-20 20:00:39 +0900

    setting

---
+++ 2018_Summer_Winter/Skill_tree/49993.py
@@ -0,0 +1,9 @@
+def solution(skill, skill_trees):
+    answer = -1
+    return answer
+
+if __name__ == '__main__' :
+    skill = "CBD"
+    skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
+
+    print(solution(skill, skill_trees))
