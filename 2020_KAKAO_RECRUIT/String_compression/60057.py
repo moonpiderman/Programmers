db9225675768c0cbefeab9f676546f04d398bd33
moonpiderman <lotsofchuck@gmail.com>
2021-06-24 11:35:07 +0900

    string compression

--- 2020_KAKAO_RECRUIT/String_compression/60057.py
+++ 2020_KAKAO_RECRUIT/String_compression/60057.py
@@ -1,6 +1,30 @@
 def solution(s):
-    answer = 0
-    return answer
+    length = []
+    string = ""
+
+    if len(s) == 1 :
+        return 1
+
+    for trim in range(1, len(s) // 2 + 1) :
+        count = 1
+        tmp = s[:trim]
+        for i in range(trim, len(s), trim) :
+            if s[i : i+trim] == tmp :
+                count += 1
+            else :
+                if count == 1:
+                    # 하나 짜리는 카운팅해줄 필요 없음
+                    count = ""
+                string += str(count) + tmp
+                tmp = s[i : i+trim]
+                count = 1
+        if count == 1 :
+            count = ""
+        string += str(count) + tmp
+        length.append(len(string))
+        string = ""
+
+    return min(length)
 
 if __name__ == '__main__' :
     s_1 = "aabbaccc"
@@ -9,4 +33,4 @@ if __name__ == '__main__' :
     s_4 = "abcabcabcabcdededededede"
     s_5 = "xababcdcdababcdcd"
 
-    print(solution(s_1))
+    print(solution(s_2))
