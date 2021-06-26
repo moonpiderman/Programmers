61e021ee6baeb5b9f4e72733dae5893eaddfa34a
moonpiderman <lotsofchuck@gmail.com>
2021-06-26 20:46:09 +0900

    fin

--- Practice/Collatz/12943.py
+++ Practice/Collatz/12943.py
@@ -1,5 +1,17 @@
 def solution(num):
     answer = 0
+
+    while (num > 1) :
+        if num % 2 == 0 :
+            num /= 2
+            answer += 1
+        else :
+            num = num * 3 + 1
+            answer += 1
+
+        if answer >= 500 :
+            answer = -1
+            break
     return answer
 
 if __name__ == '__main__' :
@@ -7,4 +19,4 @@ if __name__ == '__main__' :
     n_2 = 16
     n_3 = 626331
 
-    print(solution(n_1))
+    print(solution(n_3))
