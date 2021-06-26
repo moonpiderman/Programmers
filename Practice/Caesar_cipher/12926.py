9e6795eb8ccc8dd267c59abd753fbe645a84c1f5
moonpiderman <lotsofchuck@gmail.com>
2021-06-26 18:35:56 +0900

    start

---
+++ Practice/Caesar_cipher/12926.py
@@ -0,0 +1,14 @@
+def solution(s, n):
+    answer = ''
+    return answer
+
+
+if __name__ == '__main__' :
+    s_1 = "AB"
+    n_1 = 1
+    s_2 = "z"
+    n_2 = 1
+    s_3 = "a B z"
+    n_3 = 4
+
+    print(solution(s_1, n_1))
