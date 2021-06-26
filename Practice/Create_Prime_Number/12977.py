848e297140981081e7756b522729a0f060751a6b
moonpiderman <lotsofchuck@gmail.com>
2021-06-26 20:14:38 +0900

    fin

--- Practice/Create_Prime_Number/12977.py
+++ Practice/Create_Prime_Number/12977.py
@@ -1,5 +1,24 @@
+from itertools import combinations
+import math
+
+def prime(number) :
+    for i in range(2, int(math.sqrt(number)) + 1) :
+        if number % i == 0 :
+            return 0
+    return 1
+
 def solution(nums):
     answer = 0
+    result = []
+    nums = list(set(nums))
+    com = list(combinations(nums, 3))
+
+    for i in range(len(com)) :
+        result.append(sum(com[i]))
+
+    for j in result :
+        if prime(j) == 1 :
+            answer += 1
 
     return answer
 
