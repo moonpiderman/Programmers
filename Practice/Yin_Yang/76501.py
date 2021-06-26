b21f023b0056735f2f50faca62be619ae3d1869e
moonpiderman <lotsofchuck@gmail.com>
2021-06-26 19:17:51 +0900

    start

---
+++ Practice/Yin_Yang/76501.py
@@ -0,0 +1,12 @@
+def solution(absolutes, signs):
+    answer = 123456789
+    return answer
+
+if __name__ == '__main__' :
+    absolutes_1 = [4, 7, 12]
+    signs_1 = [True, False, True]
+
+    absolutes_2 = [1, 2, 3]
+    signs_2 = [False, False, True]
+
+    print(solution(absolutes_1, signs_1))
