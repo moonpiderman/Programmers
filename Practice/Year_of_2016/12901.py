f31bf0b071ea125d77b95db95961610b5d9e7175
moonpiderman <lotsofchuck@gmail.com>
2021-06-27 14:10:22 +0900

    fin

--- Practice/Year_of_2016/12901.py
+++ Practice/Year_of_2016/12901.py
@@ -1,6 +1,8 @@
+import datetime
 def solution(a, b):
-    answer = ''
-    return answer
+    today = 'MON TUE WED THU FRI SAT SUN'.split()
+
+    return today[datetime.datetime(2016, a, b).weekday()]
 
 if __name__ == '__main__' :
     a = 5
