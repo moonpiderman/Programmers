06573408211b4998d6491b909db5ccd99c6ec92a
moonpiderman <lotsofchuck@gmail.com>
2021-06-28 16:06:53 +0900

    fin

--- Practice/Scalar/70128.py
+++ Practice/Scalar/70128.py
@@ -1,13 +1,18 @@
 def solution(a, b):
-    answer = 1234567890
+    answer = 0
+    length = len(a)
+
+    for i in range(length) :
+        answer += (a[i] * b[i])
+
     return answer
 
 if __name__ == '__main__' :
-    a_1 = [1,2,3,4]
-    b_1 = [-3,-1,0,2]
+    a_1 = [1, 2, 3, 4]
+    b_1 = [-3, -1, 0, 2]
 
-    a_2 = [-1,0,1]
-    b_2 = [1,0,-1]
+    a_2 = [-1, 0, 1]
+    b_2 = [1, 0, -1]
 
     print(solution(a_1, b_1))
-    # print(solution(a_2, b_2))
+    print(solution(a_2, b_2))
