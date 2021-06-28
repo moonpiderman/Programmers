bdddeb45e713431bf7d2a60c5308bf649d70f47d
moonpiderman <lotsofchuck@gmail.com>
2021-06-28 16:33:29 +0900

    re-edit

--- Practice/Phoneketmon/1845.py
+++ Practice/Phoneketmon/1845.py
@@ -1,8 +1,10 @@
 def solution(nums):
     answer = 0
+
+
     return answer
 
 if __name__ == '__main__' :
-    nums_1 = [3,1,2,3]
-    nums_2 = [3,3,3,2,2,4]
-    nums_3 = [3,3,3,2,2,2]
+    nums_1 = [3, 1, 2, 3]
+    nums_2 = [3, 3, 3, 2, 2, 4]
+    nums_3 = [3, 3, 3, 2, 2, 2]
