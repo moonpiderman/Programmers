255d952cda80e91a749c791a3d3056f5aad5c439
moonpiderman <lotsofchuck@gmail.com>
2021-06-28 19:33:01 +0900

    fin

--- Practice/Phoneketmon/1845.py
+++ Practice/Phoneketmon/1845.py
@@ -1,27 +1,50 @@
 from itertools import combinations
-def solution(nums):
+def solution_fail(nums):
     answer = []
-    l = list(combinations(nums, len(nums) // 2))
-    length = len(l)
+    ball = []
+    poke = len(nums) // 2
+    l = list(combinations(nums, poke))
+    print(l)
+    # print(poke)
 
-    for i in range(length) :
+    for i in l :
         tmp = []
-        # 내부 전부 다 중복체크해야함 시부럴거
-        if l[i][0] != l[i][1] :
-            tmp.append(l[i][0])
-            tmp.append(l[i][1])
-        else :
-            continue
-        tmp.sort()
+        # for j in range(poke) :
+        #
+        ball.append(tmp)
 
-        if not tmp in answer :
-            answer.append(tmp)
+    print(answer)
 
-    return len(answer[0])
+    return len(answer)
+
+def solution_diff(nums) :
+    answer = 0
+    length = len(nums) // 2
+    nums.sort()
+    last = 0
+
+    for p in nums :
+        if (p != last and answer < length) :
+            answer += 1
+            last = p
+
+    return answer
+
+def solution(nums) :
+    answer = 0
+    pick = len(nums) // 2
+    nums = list(set(nums))
+
+    if len(nums) >= pick :
+        answer = pick
+    elif len(nums) < pick :
+        answer = len(nums)
+
+    return answer
 
 if __name__ == '__main__' :
     nums_1 = [3, 1, 2, 3]
     nums_2 = [3, 3, 3, 2, 2, 4]
     nums_3 = [3, 3, 3, 2, 2, 2]
 
-    print(solution(nums_2))
+    print(solution(nums_1))
