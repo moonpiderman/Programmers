cb976d58ceab56be184e9735e29b437b19035976
moonpiderman <lotsofchuck@gmail.com>
2021-06-28 20:08:53 +0900

    start

---
+++ 2019_KAKAO_RECRUIT/Failure_rate/42889.py
@@ -0,0 +1,12 @@
+def solution(N, stages):
+    answer = []
+    return answer
+
+
+if __name__ == '__main__' :
+    N_1 = 5
+    stages_1 = [2, 1, 2, 6, 2, 4, 3, 3]
+    N_2 = 4
+    stages_2 = [4, 4, 4, 4, 4]
+
+    print(solution(N_1, stages_1))
