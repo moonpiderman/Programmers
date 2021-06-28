27d57d8b853f804f5c9f22bdbf2a001e09260f3a
moonpiderman <lotsofchuck@gmail.com>
2021-06-28 13:08:47 +0900

    start

---
+++ Before_2018_SW/Budget/12982.py
@@ -0,0 +1,12 @@
+def solution(d, budget):
+    answer = 0
+    return answer
+
+if __name__ == '__main__' :
+    d_1 = [1, 3, 2, 5, 4]
+    budget_1 = 9
+    d_2 = [2, 2, 2, 3]
+    budget_2 = 10
+
+    print(solution(d_1, budget_1))
+    # print(solution(d_2, budget_2))
