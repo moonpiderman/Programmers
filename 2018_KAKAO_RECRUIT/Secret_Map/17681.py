cd04bfa7a3c6877f97eb91aaeeadb312bb8d3ac4
moonpiderman <lotsofchuck@gmail.com>
2021-06-28 15:57:10 +0900

    fin

--- 2018_KAKAO_RECRUIT/Secret_Map/17681.py
+++ 2018_KAKAO_RECRUIT/Secret_Map/17681.py
@@ -1,5 +1,21 @@
 def solution(n, arr1, arr2):
-    answer = []
+    answer = [ "" for r in range(n) ]
+    lst_1 = []
+    lst_2 = []
+
+    for i in range(n) :
+        res1 = '{0:b}'.format(arr1[i]).zfill(n)
+        res2 = '{0:b}'.format(arr2[i]).zfill(n)
+        lst_1.append(res1)
+        lst_2.append(res2)
+
+    for i in range(n) :
+        for j in range(n) :
+            if lst_1[i][j] == '0' and lst_2[i][j] == '0' :
+                answer[i] += " "
+            else :
+                answer[i] += "#"
+
     return answer
 
 if __name__ == '__main__' :
@@ -11,5 +27,5 @@ if __name__ == '__main__' :
     arr1_2 = [46, 33, 33 ,22, 31, 50]
     arr2_2 = [27 ,56, 19, 14, 14, 10]
 
-    print(solution(n_1, arr1_1, arr2_1))
-    # print(solution(n_2, arr1_2, arr2_2))
+    # print(solution(n_1, arr1_1, arr2_1))
+    print(solution(n_2, arr1_2, arr2_2))
