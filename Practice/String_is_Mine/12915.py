778fa23fff59408b53bcfd6c65504bc3e36548ce
moonpiderman <lotsofchuck@gmail.com>
2021-06-28 12:03:02 +0900

    start

---
+++ Practice/String_is_Mine/12915.py
@@ -0,0 +1,12 @@
+def solution(strings, n):
+    answer = []
+    return answer
+
+if __name__ == '__main__' :
+    strings_1 = ["sun", "bed", "car"]
+    n_1 = 1
+    strings_2 = ["abce", "abcd", "cdx"]
+    n_2 = 2
+
+    print(solution(strings_1, n_1))
+    # print(solution(strings_2, n_2))
