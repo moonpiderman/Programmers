7d51e6ab495179934f5a966db8b6532db9e7e346
moonpiderman <lotsofchuck@gmail.com>
2021-06-28 13:56:59 +0900

    start

---
+++ Practice/Matrix_sum/12950.py
@@ -0,0 +1,13 @@
+def solution(arr1, arr2):
+    answer = [[]]
+    return answer
+
+if __name__ == '__main__' :
+    arr1_1 = [[1, 2], [2, 3]]
+    arr2_1 = [[3, 4], [5, 6]]
+
+    arr1_2 = [[1], [2]]
+    arr2_2 = [[3], [4]]
+
+    print(solution(arr1_1, arr2_1))
+    # print(solution(arr1_2, arr2_2))
