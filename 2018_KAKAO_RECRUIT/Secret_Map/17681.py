2a89edfd89caf13370808b3365b983b094d08798
moonpiderman <lotsofchuck@gmail.com>
2021-06-28 14:20:59 +0900

    start

---
+++ 2018_KAKAO_RECRUIT/Secret_Map/17681.py
@@ -0,0 +1,15 @@
+def solution(n, arr1, arr2):
+    answer = []
+    return answer
+
+if __name__ == '__main__' :
+    n_1 = 5
+    arr1_1 = [9, 20, 28, 18, 11]
+    arr2_1 = [30, 1, 21, 17, 28]
+
+    n_2 = 6
+    arr1_2 = [46, 33, 33 ,22, 31, 50]
+    arr2_2 = [27 ,56, 19, 14, 14, 10]
+
+    print(solution(n_1, arr1_1, arr2_1))
+    # print(solution(n_2, arr1_2, arr2_2))
