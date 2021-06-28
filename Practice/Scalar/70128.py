4f74370ca4165d10d3b04383cf023ae63544f2d8
moonpiderman <lotsofchuck@gmail.com>
2021-06-28 16:02:42 +0900

    start

---
+++ Practice/Scalar/70128.py
@@ -0,0 +1,13 @@
+def solution(a, b):
+    answer = 1234567890
+    return answer
+
+if __name__ == '__main__' :
+    a_1 = [1,2,3,4]
+    b_1 = [-3,-1,0,2]
+
+    a_2 = [-1,0,1]
+    b_2 = [1,0,-1]
+
+    print(solution(a_1, b_1))
+    # print(solution(a_2, b_2))
