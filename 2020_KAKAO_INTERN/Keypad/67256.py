ec3d1a68bd29c5f8d2adacbe159d9a8e4f80d10a
moonpiderman <lotsofchuck@gmail.com>
2021-06-29 13:08:07 +0900

    start

---
+++ 2020_KAKAO_INTERN/Keypad/67256.py
@@ -0,0 +1,15 @@
+def solution(numbers, hand):
+    answer = ''
+    return answer
+
+if __name__ == '__main__' :
+    numbers_1 = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
+    hand_1 = "right"
+
+    numbers_2 = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]
+    hand_2 = "left"
+
+    numbers_3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    hand_3 = "right"
+
+    print(solution(numbers_1, hand_1))
