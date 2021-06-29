f3ae8e7508bc3708ed600b7da3ea4b7c571e27ec
moonpiderman <lotsofchuck@gmail.com>
2021-06-29 22:19:02 +0900

    start

---
+++ 2018_KAKAO_RECRUIT/Dart/17682.py
@@ -0,0 +1,12 @@
+def solution(dartResult):
+    answer = 0
+    return answer
+
+if __name__ == '__main__' :
+    dartResult_1 = "1S2D*3T"
+    dartResult_2 = "1D2S#10S"
+    dartResult_3 = "1D2S0T"
+    dartResult_4 = "1S*2T*3S"
+    dartResult_5 = "1D#2S*3S"
+    dartResult_6 = "1T2D3D#"
+    dartResult_7 = "1D2S3T*"
