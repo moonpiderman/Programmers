fa3337e47a0e33ad4f3eb8b977fa28e53f8f2d82
moonpiderman <lotsofchuck@gmail.com>
2021-06-29 22:03:57 +0900

    fin

--- Practice/Base_3/68935.py
+++ Practice/Base_3/68935.py
@@ -1,5 +1,20 @@
 def solution(n):
     answer = 0
+    base_3 = ''
+    tmp = ''
+    base = [0, 1, 2]
+
+    while n :
+        base_3 = str(base[n % 3]) + base_3
+        if n % 3 == 0 :
+            n = (n // 3)
+        else :
+            n = n // 3
+    base_3 = list(base_3)
+
+    for i in range(len(base_3)) :
+        answer += int(base_3[i]) * pow(3, i)
+
     return answer
 
 if __name__ == '__main__' :
@@ -7,4 +22,4 @@ if __name__ == '__main__' :
     n_2 = 125
 
     print(solution(n_1))
-    print(solution(n_2))
+    # print(solution(n_2))
