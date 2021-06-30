2522339c75d7ca67c850ae28faae78d0c66ac167
moonpiderman <lotsofchuck@gmail.com>
2021-06-30 10:12:10 +0900

    start

---
+++ 2021_DEV_MATCHING/Lotto/77484.py
@@ -0,0 +1,17 @@
+def solution(lottos, win_nums):
+    answer = []
+    return answer
+
+if __name__ == '__main__' :
+    lottos_1 = [44, 1, 0, 0, 31, 25]
+    win_nums_1 = [31, 10, 45, 1, 6, 19]
+
+    lottos_2 = [0, 0, 0, 0, 0, 0]
+    win_nums_2 = [38, 19, 20, 40, 15, 25]
+
+    lottos_3 = [45, 4, 35, 20, 3, 9]
+    win_nums_3 = [20, 9, 3, 45, 4, 35]
+
+    print(solution(lottos_1, win_nums_1))
+    print(solution(lottos_2, win_nums_2))
+    print(solution(lottos_3, win_nums_3))
