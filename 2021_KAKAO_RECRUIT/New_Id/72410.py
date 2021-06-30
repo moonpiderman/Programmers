6a940d6b6b7a95d405d46443051f1da364df0c3c
moonpiderman <lotsofchuck@gmail.com>
2021-06-30 16:27:56 +0900

    start

---
+++ 2021_KAKAO_RECRUIT/New_Id/72410.py
@@ -0,0 +1,10 @@
+def solution(new_id):
+    answer = ''
+    return answer
+
+if __name__ == '__main__' :
+    new_id_1 = "...!@BaT#*..y.abcdefghijklm"
+    new_id_2 = "z-+.^."
+    new_id_3 = "=.="
+    new_id_4 = "123_.def"
+    new_id_5 = "abcdefghijklmn.p"
