3ab3a61424c02560e7516a51267f76fb090311cc
moonpiderman <lotsofchuck@gmail.com>
2021-06-30 16:16:38 +0900

    shit

--- 2020_KAKAO_INTERN/Keypad/67256.py
+++ 2020_KAKAO_INTERN/Keypad/67256.py
@@ -1,66 +1,65 @@
 import math
-
 def distance_check(l_loca, r_loca, target, hand):
     left = l_loca
     right = r_loca
-    l_distance = math.sqrt(pow(abs(target[0] - left[0]), 2) + pow(abs(target[1] - left[1]), 2))
-    r_distance = math.sqrt(pow(abs(target[0] - right[0]), 2) + pow(abs(target[1] - right[1]), 2))
+    t = target
+    h = hand
+    finger = ""
+    l_distance = math.sqrt(math.pow(abs(t[0] - left[0]), 2) + math.pow(abs(t[1] - left[1]), 2))
+    r_distance = math.sqrt(math.pow(abs(t[0] - right[0]), 2) + math.pow(abs(t[1] - right[1]), 2))
 
     if l_distance == r_distance :
-        if hand == "left" :
+        if h == "left" :
             finger = "L"
-            left = target
-        elif hand == "right" :
+            left = t
+        elif h == "right" :
             finger = "R"
-            right = target
+            right = t
 
     elif l_distance > r_distance :
         finger = "R"
-        right = target
+        right = t
 
     elif r_distance > l_distance :
         finger = "L"
-        left = target
+        left = t
 
     return left, right, finger
 
 def solution(numbers, hand):
     answer = ''
     keypad = {
-        "1": [0, 0],
-        "2": [0, 1],
-        "3": [0, 2],
-        "4": [1, 0],
-        "5": [1, 1],
-        "6": [1, 2],
-        "7": [2, 0],
-        "8": [2, 1],
-        "9": [2, 2],
-        "*": [3, 0],
-        "0": [3, 1],
-        "#": [3, 2]
+        '1': [0, 0],
+        '2': [0, 1],
+        '3': [0, 2],
+        '4': [1, 0],
+        '5': [1, 1],
+        '6': [1, 2],
+        '7': [2, 0],
+        '8': [2, 1],
+        '9': [2, 2],
+        '*': [3, 0],
+        '0': [3, 1],
+        '#': [3, 2],
     }
     # 초기 위치설정
-    l_loca = keypad["*"]
-    r_loca = keypad["#"]
-
-    print("첫 위치 :", l_loca, r_loca)
+    l_location = keypad["*"]
+    r_location = keypad["#"]
 
     for num in numbers :
         target = keypad[str(num)]
         if num == 1 or num == 4 or num == 7 :
-            l_loca[0] = target[0]
-            l_loca[1] = target[1]
+            l_location[0] = target[0]
+            l_location[1] = target[1]
             finger = "L"
         elif num == 3 or num == 6 or num == 9 :
-            r_loca[0] = target[0]
-            r_loca[1] = target[1]
+            r_location[0] = target[0]
+            r_location[1] = target[1]
             finger = "R"
         else :
-            l_loca, r_loca, finger = distance_check(l_loca, r_loca, target, hand)
+            l_location, r_location, finger = distance_check(l_location, r_location, target, hand)
 
         answer += finger
-        print(l_loca, r_loca)
     return answer
 
 if __name__ == '__main__' :
