b62020bc660cb180f55ecd13b10b49b807e25be1
moonpiderman <lotsofchuck@gmail.com>
2021-06-30 17:00:07 +0900

    st

--- 2021_KAKAO_RECRUIT/New_Id/72410.py
+++ 2021_KAKAO_RECRUIT/New_Id/72410.py
@@ -1,5 +1,8 @@
 def solution(new_id):
     answer = ''
+    new_id = list(new_id)
+    print(new_id)
+
     return answer
 
 if __name__ == '__main__' :
@@ -8,3 +11,9 @@ if __name__ == '__main__' :
     new_id_3 = "=.="
     new_id_4 = "123_.def"
     new_id_5 = "abcdefghijklmn.p"
+
+    print(solution(new_id_1))
+    # print(solution(new_id_2))
+    # print(solution(new_id_3))
+    # print(solution(new_id_4))
+    # print(solution(new_id_5))
