94ae80d550b1adc832c7ef683e773cff7919273b
moonpiderman <lotsofchuck@gmail.com>
2021-07-03 22:32:45 +0900

    start

---
+++ 2018_KAKAO_RECRUIT/Cache/17680.py
@@ -0,0 +1,29 @@
+def solution(cacheSize, cities):
+    answer = 0
+    return answer
+
+if __name__ == '__main__' :
+    cacheSize_1 = 3
+    cities_1 = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]
+
+    cacheSize_2 = 3
+    cities_2 = ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]
+
+    cacheSize_3 = 2
+    cities_3 = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]
+
+    cacheSize_4 = 5
+    cities_4 = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]
+
+    cacheSize_5 = 2
+    cities_5 = ["Jeju", "Pangyo", "NewYork", "newyork"]
+
+    cacheSize_6 = 0
+    cities_6 = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]
+
+    print(solution(cacheSize_1, cities_1))
+    # print(solution(cacheSize_2, cities_2))
+    # print(solution(cacheSize_3, cities_3))
+    # print(solution(cacheSize_4, cities_4))
+    # print(solution(cacheSize_5, cities_5))
+    # print(solution(cacheSize_6, cities_6))
