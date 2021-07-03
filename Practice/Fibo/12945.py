366a1938629d6fde6f2677050b46528855dc9486
moonpiderman <lotsofchuck@gmail.com>
2021-07-03 20:33:16 +0900

    fin fibo

--- Practice/Fibo/12945.py
+++ Practice/Fibo/12945.py
@@ -1,10 +1,19 @@
-def fibo(n) :
+def fibo_recursive(n) :
     if n == 0 :
         return 0
     elif n == 1 :
         return 1
     elif n >= 2 :
-        return fibo(n-1) + fibo(n-2)
+        return fibo_recursive(n-1) + fibo_recursive(n-2)
+
+def fibo(n) :
+    if n < 2 :
+        return n
+    a, b = 0, 1
+    for i in range(n - 1) :
+        a, b = b, a + b
+
+    return b
 
 def solution(n):
     answer = fibo(n)
