da9aa7e209bdd624eb4ff292102d9da7dc9ca1e4
moonpiderman <lotsofchuck@gmail.com>
2021-07-03 22:23:59 +0900

    fin

--- Practice/Max_Min/12939.py
+++ Practice/Max_Min/12939.py
@@ -1,5 +1,13 @@
 def solution(s):
     answer = ''
+    array = s.split(' ')
+
+    for i in range(len(array)) :
+        array[i] = int(array[i])
+
+    answer += str(min(array)) + ' '
+    answer += str(max(array))
+
     return answer
 
 if __name__ == '__main__' :
@@ -10,4 +18,3 @@ if __name__ == '__main__' :
     print(solution(s_1))
     # print(solution(s_2))
     # print(solution(s_3))
-    # print(solution(s_4))
