8201a9a29ebe47f777bf780d5a7830ecd28f3fdc
moonpiderman <lotsofchuck@gmail.com>
2021-07-03 20:42:39 +0900

    start

---
+++ Greedy/Boat/42885.py
@@ -0,0 +1,13 @@
+def solution(people, limit):
+    answer = 0
+    return answer
+
+if __name__ == '__main__' :
+    people_1 = [70, 50, 80, 50]
+    limit_1 = 100
+
+    people_2 = [70, 80, 50]
+    limit_2 = 100
+
+    print(solution(people_1, limit_1))
+    # print(solution(people_2, limit_2))
