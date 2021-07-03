5880f52ffd77f009ebba7d853cb9b8538f6cf57d
moonpiderman <lotsofchuck@gmail.com>
2021-07-03 22:18:54 +0900

    start

---
+++ Practice/Max_Min/12939.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = ''
+    return answer
+
+if __name__ == '__main__' :
+    s_1 = "1 2 3 4"
+    s_2 = "-1 -2 -3 -4"
+    s_3 = '-1 -1'
+
+    print(solution(s_1))
+    # print(solution(s_2))
+    # print(solution(s_3))
+    # print(solution(s_4))
