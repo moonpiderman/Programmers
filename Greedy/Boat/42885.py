252c0e16df0c46d3b4c87702c7842e06c7600bc4
moonpiderman <lotsofchuck@gmail.com>
2021-07-03 21:02:18 +0900

    fail

--- Greedy/Boat/42885.py
+++ Greedy/Boat/42885.py
@@ -1,4 +1,19 @@
-def solution(people, limit):
+from itertools import combinations
+def solution_fail(people, limit):
+    answer = 0
+    com = list(combinations(people, 2))
+
+    for i in range(len(com)) :
+        if com[i][0] + com[i][1] <= limit :
+            people.remove(com[i][0])
+            people.remove(com[i][1])
+            answer += 1
+
+    answer += len(people)
+
+    return answer
+
+def solution(people, limit) :
     answer = 0
     return answer
 
@@ -10,4 +25,4 @@ if __name__ == '__main__' :
     limit_2 = 100
 
     print(solution(people_1, limit_1))
-    # print(solution(people_2, limit_2))
+    print(solution(people_2, limit_2))
