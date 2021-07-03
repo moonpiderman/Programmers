f42f1989ea2338485f39c1d9994d60c4da9bee1d
moonpiderman <lotsofchuck@gmail.com>
2021-07-03 21:42:18 +0900

    fin

--- Greedy/Boat/42885.py
+++ Greedy/Boat/42885.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from collections import deque
 def solution_fail(people, limit):
     answer = 0
     com = list(combinations(people, 2))
@@ -14,7 +15,39 @@ def solution_fail(people, limit):
     return answer
 
 def solution(people, limit) :
+    answer = len(people)
+    ppl = sorted(people, reverse=True)
+
+    sp, ep = 0, len(ppl) - 1
+
+    while sp < ep :
+        if ppl[sp] + ppl[ep] <= limit :
+            ep -= 1
+            answer -= 1
+        sp += 1
+
+    return answer
+
+def solution_mine(people, limit) :
     answer = 0
+    people.sort()
+    que = deque(people)
+
+    while que :
+        if len(que) >= 2 :
+            if que[0] + que[-1] <= limit:
+                que.pop()
+                que.popleft()
+                answer += 1
+            elif que[0] + que[-1] > limit:
+                que.pop()
+                answer += 1
+        else :
+            if que[0] <= limit :
+                que.pop()
+                answer += 1
+
+
     return answer
 
 if __name__ == '__main__' :
@@ -24,5 +57,7 @@ if __name__ == '__main__' :
     people_2 = [70, 80, 50]
     limit_2 = 100
 
-    print(solution(people_1, limit_1))
-    print(solution(people_2, limit_2))
+    # print(solution(people_1, limit_1))
+    # print(solution(people_2, limit_2))
+    print(solution_mine(people_1, limit_1))
+    print(solution_mine(people_2, limit_2))
