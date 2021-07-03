479f6e002929b3bf8912409bf6633d38026137a9
moonpiderman <lotsofchuck@gmail.com>
2021-07-03 22:15:41 +0900

    fin

--- Practice/Representation_for_Number/12924.py
+++ Practice/Representation_for_Number/12924.py
@@ -1,6 +1,17 @@
 def solution(n):
     answer = 0
 
+    for i in range(1, n + 1) :
+        chk = 0
+        for j in range(i, n + 1) :
+            chk += j
+            if chk < n :
+                continue
+            elif chk == n :
+                answer += 1
+                break
+            else :
+                break
 
     return answer
 
