5364fb1f57f1c16cda0595dd4d2abeab872025a9
moonpiderman <lotsofchuck@gmail.com>
2021-07-04 19:24:56 +0900

    fin

--- Practice/Oriental_Game/12913.py
+++ Practice/Oriental_Game/12913.py
@@ -1,7 +1,29 @@
-def solution(land):
+def solution_fail(land):
     answer = 0
+    row = len(land)
 
+    for i in range(row) :
+        for j in range(4) :
+            if i == 0 :
+                if max(land[i]) == land[i][j] :
+                    stop = j
+                    break
+            else :
+                land[i][stop] = 0
+                if max(land[i]) == land[i][j] :
+                    stop = j
+                    break
+        answer += max(land[i])
     return answer
+
+def solution(land) :
+    n = len(land) - 1
+    for i in range(n) :
+        land[i + 1][0] += max(land[i][1], land[i][2], land[i][3])
+        land[i + 1][1] += max(land[i][0], land[i][2], land[i][3])
+        land[i + 1][2] += max(land[i][0], land[i][1], land[i][3])
+        land[i + 1][3] += max(land[i][0], land[i][1], land[i][2])
+    return max(land[n])
 
 if __name__ == '__main__' :
     land = [[1, 2, 3, 5],
