1e6254ec4ecd4ba6d84ac614555c518d69d847ff
moonpiderman <lotsofchuck@gmail.com>
2021-07-04 18:45:40 +0900

    s

--- Practice/Oriental_Game/12913.py
+++ Practice/Oriental_Game/12913.py
@@ -4,6 +4,8 @@ def solution(land):
     return answer
 
 if __name__ == '__main__' :
-    land = [[1,2,3,5],[5,6,7,8],[4,3,2,1]]
+    land = [[1, 2, 3, 5],
+            [5, 6, 7, 8],
+            [4, 3, 2, 1]]
 
     print(solution(land))
