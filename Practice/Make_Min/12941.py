9b4c51907bf0066575c34b9f36abb25b791ff75c
moonpiderman <lotsofchuck@gmail.com>
2021-07-04 16:09:22 +0900

    start

---
+++ Practice/Make_Min/12941.py
@@ -0,0 +1,14 @@
+def solution(A, B):
+    answer = 0
+
+    return answer
+
+if __name__ == '__main__' :
+    A_1 = [1, 4, 2]
+    B_1 = [5, 4, 4]
+
+    A_2 = [1, 2]
+    B_2 = [3, 4]
+
+    print(solution(A_1, B_1))
+    # print(solution(A_2, B_2))
