ed1805b18e61bc22f8081aaba284ece9ec60d0a3
moonpiderman <lotsofchuck@gmail.com>
2021-07-04 18:23:10 +0900

    start

---
+++ Practice/N_GCD/12953.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = 0
+    return answer
+
+if __name__ == '__main__' :
+    arr_1 = [2, 6, 8, 14]
+    arr_2 = [1, 2, 3]
+
+    print(solution(arr_1))
+    # print(solution(arr_2))
