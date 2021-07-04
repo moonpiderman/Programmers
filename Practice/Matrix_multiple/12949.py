4ee296ad8d2abc9024b440e4c03144a69cad3b33
moonpiderman <lotsofchuck@gmail.com>
2021-07-04 17:03:32 +0900

    fin

--- Practice/Matrix_multiple/12949.py
+++ Practice/Matrix_multiple/12949.py
@@ -1,5 +1,19 @@
 def solution(arr1, arr2):
-    answer = [[]]
+    answer = []
+    answer_row = len(arr1)
+    answer_col = len(arr2[0])
+
+    for i in range(answer_row) :
+        tmp = []
+        for j in range(answer_col) :
+            result = 0
+            for k in range(len(arr1[0])) :
+                val1 = arr1[i][k]
+                val2 = arr2[k][j]
+                result += val1 * val2
+            tmp.append(result)
+        answer.append(tmp)
+
     return answer
 
 if __name__ == '__main__' :
@@ -10,4 +24,4 @@ if __name__ == '__main__' :
     arr2_2 = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]
 
     print(solution(arr1_1, arr2_1))
-    # print(solution(arr1_2, arr2_2))
+    print(solution(arr1_2, arr2_2))
