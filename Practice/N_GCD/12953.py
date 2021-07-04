e918943906c133cf0bc7215da84538f67f53128f
moonpiderman <lotsofchuck@gmail.com>
2021-07-04 18:39:26 +0900

    fin

--- Practice/N_GCD/12953.py
+++ Practice/N_GCD/12953.py
@@ -1,5 +1,15 @@
 def solution(arr):
     answer = 0
+    arr = sorted(arr, reverse=True)
+
+    for i in range(len(arr) - 1) :
+        for j in range(arr[i], (arr[i] * arr[i + 1]) + 1) :
+            if j % arr[i] == 0 and j % arr[i + 1] == 0 :
+                break
+        arr[i + 1] = j
+
+    answer = arr[-1]
+
     return answer
 
 if __name__ == '__main__' :
@@ -7,4 +17,4 @@ if __name__ == '__main__' :
     arr_2 = [1, 2, 3]
 
     print(solution(arr_1))
-    # print(solution(arr_2))
+    print(solution(arr_2))
