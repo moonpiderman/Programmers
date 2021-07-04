e7fbf800981022875ac5f8172902a6f7808e3dcd
moonpiderman <lotsofchuck@gmail.com>
2021-07-04 14:35:52 +0900

    start

---
+++ Practice/Correct_Parentheses/12909.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = True
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return True
+
+if __name__ == '__main__' :
+    s_1 = "()()"
+    s_2 = "(())()"
+    s_3 = ")()("
+    s_4 = "(()("
+
+    print(solution(s_1))
+    # print(solution(s_2))
+    # print(solution(s_3))
+    # print(solution(s_4))
