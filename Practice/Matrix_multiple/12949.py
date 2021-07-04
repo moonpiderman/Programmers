268315821f31080f21754b7e939ba49543a9fb47
moonpiderman <lotsofchuck@gmail.com>
2021-07-04 16:25:51 +0900

    start

---
+++ Practice/Matrix_multiple/12949.py
@@ -0,0 +1,13 @@
+def solution(arr1, arr2):
+    answer = [[]]
+    return answer
+
+if __name__ == '__main__' :
+    arr1_1 = [[1, 4], [3, 2], [4, 1]]
+    arr2_1 = [[3, 3], [3, 3]]
+
+    arr1_2 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
+    arr2_2 = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]
+
+    print(solution(arr1_1, arr2_1))
+    # print(solution(arr1_2, arr2_2))
