f4c85fa6eb489cf27eeff4f64b53909d9af3ae13
moonpiderman <lotsofchuck@gmail.com>
2021-07-05 10:28:57 +0900

    fin

--- 2019_KAKAO_WINTER_INTERN/Tuple/64065.py
+++ 2019_KAKAO_WINTER_INTERN/Tuple/64065.py
@@ -1,16 +1,26 @@
 def solution(s):
-    answer = []
-    return answer
+    stack = []
+
+    s = s[2:-2]
+    s = s.split('},{')
+    s.sort(key=len)
+
+    for i in s :
+        spy = i.split(',')
+        for j in spy :
+            if not int(j) in stack :
+                stack.append(int(j))
+    return stack
 
 
 if __name__ == '__main__' :
-    s_1 = {{2}, {2, 1}, {2, 1, 3}, {2, 1, 3, 4}}
-    s_2 = {{1, 2, 3}, {2, 1}, {1, 2, 4, 3}, {2}}
-    s_3 = {{20, 111}, {111}}
+    s_1 = {{2},{2, 1},{2, 1, 3},{2, 1, 3, 4}}
+    s_2 = {{1, 2, 3},{2, 1},{1, 2, 4, 3},{2}}
+    s_3 = {{20, 111},{111}}
     s_4 = {{123}}
-    s_5 = {{4, 2, 3}, {3}, {2, 3, 4, 1}, {2, 3}}
+    s_5 = {{4, 2, 3},{3},{2, 3, 4, 1},{2, 3}}
 
-    print(solution(s_1))
+    # print(solution(s_1))
     # print(solution(s_2))
     # print(solution(s_3))
     # print(solution(s_4))
