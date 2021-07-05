30a0bc26450163f93df359bcaa907a3ff5def9cc
moonpiderman <lotsofchuck@gmail.com>
2021-07-05 17:13:37 +0900

    start

---
+++ Before_2018_SW/Jump/12980.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    return answer
+
+if __name__ == '__main__' :
+    n_1 = 5
+    n_2 = 6
+    n_3 = 5000
+
+    print(solution(n_1))
+    print(solution(n_2))
+    print(solution(n_3))
