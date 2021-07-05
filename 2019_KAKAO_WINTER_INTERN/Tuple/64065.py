9ee20e97ebacf05293a064dfc5fd0fda3e910e41
moonpiderman <lotsofchuck@gmail.com>
2021-07-05 10:01:27 +0900

    start

---
+++ 2019_KAKAO_WINTER_INTERN/Tuple/64065.py
@@ -0,0 +1,17 @@
+def solution(s):
+    answer = []
+    return answer
+
+
+if __name__ == '__main__' :
+    s_1 = {{2}, {2, 1}, {2, 1, 3}, {2, 1, 3, 4}}
+    s_2 = {{1, 2, 3}, {2, 1}, {1, 2, 4, 3}, {2}}
+    s_3 = {{20, 111}, {111}}
+    s_4 = {{123}}
+    s_5 = {{4, 2, 3}, {3}, {2, 3, 4, 1}, {2, 3}}
+
+    print(solution(s_1))
+    # print(solution(s_2))
+    # print(solution(s_3))
+    # print(solution(s_4))
+    # print(solution(s_5))
