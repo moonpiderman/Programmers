4e3cf4bccd203f3962cb3797b23908f7fe22b536
moonpiderman <lotsofchuck@gmail.com>
2021-07-05 11:47:10 +0900

    start

---
+++ 2018_KAKAO_RECRUIT/News_Clustering/17677.py
@@ -0,0 +1,21 @@
+def solution(str1, str2):
+    answer = 0
+    return answer
+
+if __name__ == '__main__' :
+    str1_1 = "FRANCE"
+    str2_1 = "french"
+
+    str1_2 = "handshake"
+    str2_2 = "shake hands"
+
+    str1_3 = "aa1+aa2"
+    str2_3 = "AAAA12"
+
+    str1_4 = "E=M*C^2"
+    str2_4 = "e=m*c^2"
+
+    print(solution(str1_1, str2_1))
+    print(solution(str1_2, str2_2))
+    print(solution(str1_3, str2_3))
+    print(solution(str1_4, str2_4))
