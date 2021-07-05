858361a02a6596d6d135e820282668b33af6a19d
moonpiderman <lotsofchuck@gmail.com>
2021-07-05 15:44:53 +0900

    fail

--- Before_2018_SW/Eng_Shiritori/12981.py
+++ Before_2018_SW/Eng_Shiritori/12981.py
@@ -1,5 +1,20 @@
 def solution(n, words):
     answer = []
+    stack = []
+    member = {}
+    for i in range(n) :
+        tmp = []
+        tmp.append(i + 1)
+        tmp.append(0)
+        member[(i+1) % n] = tmp
+
+    for i in range(len(words)) :
+        member[i % 3][1] += 1
+        if not words[i] in stack :
+            stack.append(words[i])
+    # 중복 로직 넣기
+
+    answer = member[len(words) % n]
 
     return answer
 
@@ -14,5 +29,5 @@ if __name__ == '__main__' :
     words_3 = ["hello", "one", "even", "never", "now", "world", "draw"]
 
     print(solution(n_1, words_1))
-    # print(solution(n_2, words_2))
-    # print(solution(n_3, words_3))
+    print(solution(n_2, words_2))
+    print(solution(n_3, words_3))
