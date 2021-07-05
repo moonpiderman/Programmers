82eefaa7cc0d1d6af7716d16b859fab702725682
moonpiderman <lotsofchuck@gmail.com>
2021-07-05 14:54:57 +0900

    start

---
+++ Before_2018_SW/Eng_Shiritori/12981.py
@@ -0,0 +1,18 @@
+def solution(n, words):
+    answer = []
+
+    return answer
+
+if __name__ == '__main__' :
+    n_1 = 3
+    words_1 = ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
+
+    n_2 = 5
+    words_2 = ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]
+
+    n_3 = 2
+    words_3 = ["hello", "one", "even", "never", "now", "world", "draw"]
+
+    print(solution(n_1, words_1))
+    # print(solution(n_2, words_2))
+    # print(solution(n_3, words_3))
