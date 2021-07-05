209e89036ae103745694a60e61ce791bd29a6365
moonpiderman <lotsofchuck@gmail.com>
2021-07-05 19:42:26 +0900

    fin

--- Before_2018_SW/Jump/12980.py
+++ Before_2018_SW/Jump/12980.py
@@ -2,9 +2,9 @@ def solution(n):
     answer = 0
 
     while n > 0 :
-        q, r = divmod(n, 2)
-        n = q
-        if r != 0 :
+        div, mod = divmod(n, 2)
+        n = div
+        if mod != 0 :
             answer += 1
     return answer
 
