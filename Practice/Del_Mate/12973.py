038f6557d6cef96ac9d69318316aa1817b376ca6
moonpiderman <lotsofchuck@gmail.com>
2021-07-05 11:29:59 +0900

    fin

--- Practice/Del_Mate/12973.py
+++ Practice/Del_Mate/12973.py
@@ -1,10 +1,22 @@
 def solution(s):
-    answer = -1
-    return answer
+    stack = []
+
+    for alpha in s :
+        if len(stack) == 0 :
+            stack.append(alpha)
+        elif stack[-1] == alpha :
+            stack.pop()
+        else :
+            stack.append(alpha)
+
+    if len(stack) == 0 :
+        return 1
+    else :
+        return 0
 
 if __name__ == '__main__' :
     s_1 = "baabaa"
     s_2 = "cdcd"
 
     print(solution(s_1))
-    # print(solution(s_2))
+    print(solution(s_2))
