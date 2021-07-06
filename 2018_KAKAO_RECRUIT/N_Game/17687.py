d8357fe6d5b59b2cfc3dd7ba937674a60c6afd75
moonpiderman <lotsofchuck@gmail.com>
2021-07-06 18:57:45 +0900

    start

---
+++ 2018_KAKAO_RECRUIT/N_Game/17687.py
@@ -0,0 +1,23 @@
+def solution(n, t, m, p):
+    answer = ''
+    return answer
+
+if __name__ == '__main__' :
+    n_1 = 2
+    t_1 = 4
+    m_1 = 2
+    p_1 = 1
+
+    n_2 = 16
+    t_2 = 16
+    m_2 = 2
+    p_2 = 1
+
+    n_3 = 16
+    t_3 = 16
+    m_3 = 2
+    p_3 = 2
+
+    print(solution(n_1, t_1, m_1, p_1))
+    print(solution(n_2, t_2, m_2, p_2))
+    print(solution(n_3, t_3, m_3, p_3))
