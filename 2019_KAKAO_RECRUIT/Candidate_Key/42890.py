d7d085e62a514e839daf86c3c49110c97a34fc87
moonpiderman <lotsofchuck@gmail.com>
2021-07-06 13:45:34 +0900

    fuck this is others code

--- 2019_KAKAO_RECRUIT/Candidate_Key/42890.py
+++ 2019_KAKAO_RECRUIT/Candidate_Key/42890.py
@@ -1,6 +1,29 @@
+from itertools import combinations
 def solution(relation):
-    answer = 0
-    return answer
+    row = len(relation)
+    col = len(relation[0])
+    key_idx = list(range(col))
+    candidtate_keys = []
+
+    if row == 1 : return col
+
+    for i in range(1, col + 1) :
+        for comb in combinations(key_idx, i) :
+            hist = []
+            for re in relation :
+                current_key = [re[c] for c in comb]
+                if current_key in hist :
+                    break
+                else :
+                    hist.append(current_key)
+            else :
+                for ck in candidtate_keys :
+                    if set(ck).issubset(set(comb)) :
+                        break
+                else :
+                    candidtate_keys.append(comb)
+
+    return len(candidtate_keys)
 
 
 if __name__ == '__main__' :
