51c4f5e63446c8b4e9c2484a4b4eff632e18496e
moonpiderman <lotsofchuck@gmail.com>
2021-07-06 21:14:57 +0900

    start

---
+++ Practice/Bigger_Square/12905.py
@@ -0,0 +1,19 @@
+def solution(board):
+    answer = 0
+    return answer
+
+if __name__ == '__main__' :
+    board_1 = [
+        [0, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [0, 0, 1, 0]
+    ]
+
+    board_2 = [
+        [0, 0, 1, 1],
+        [1, 1, 1, 1]
+    ]
+
+    print(solution(board_1))
+    # print(solution(board_2))
