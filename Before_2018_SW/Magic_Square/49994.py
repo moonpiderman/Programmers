12889b4d9ab254554f4e620468e24d7a4d7de146
moonpiderman <lotsofchuck@gmail.com>
2021-07-06 14:19:24 +0900

    start

---
+++ Before_2018_SW/Magic_Square/49994.py
@@ -0,0 +1,10 @@
+def solution(dirs):
+    answer = 0
+    return answer
+
+if __name__ == '__main__' :
+    dirs_1 = "ULURRDLLU"
+    dirs_2 = "LULLLLLLU"
+
+    print(solution(dirs_1))
+    # print(solution(dirs_2))
