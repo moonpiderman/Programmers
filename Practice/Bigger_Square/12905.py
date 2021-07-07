2423eef96a819fdca280f0ed31bfa3b95e663aef
moonpiderman <lotsofchuck@gmail.com>
2021-07-07 10:56:46 +0900

    fin

--- Practice/Bigger_Square/12905.py
+++ Practice/Bigger_Square/12905.py
@@ -1,9 +1,23 @@
 def solution(board):
     answer = 0
+    row = len(board)
+    col = len(board[0])
 
+    for r in board :
+        if sum(r) :
+            answer = 1
+            break
+    else :
+        return 0
 
+    for i in range(1, row) :
+        for j in range(1, col) :
+            if board[i - 1][j - 1] and board[i - 1][j] and board[i][j - 1] and board[i][j] :
+                mini = min(board[i - 1][j - 1], board[i - 1][j], board[i][j - 1]) + 1
+                board[i][j] = mini
+                answer = max(answer, board[i][j])
 
-    return answer
+    return answer ** 2
 
 if __name__ == '__main__' :
     board_1 = [
