2422e71438102a1d1c7b5fdf761dadf2be4bdc08
moonpiderman <lotsofchuck@gmail.com>
2021-07-11 20:49:12 +0900

    util Expression

---
+++ 2018_KAKAO_RECRUIT/Friends_4Block/17679.py
@@ -0,0 +1,15 @@
+def solution(m, n, board):
+    answer = 0
+    return answer
+
+if __name__ == '__main__' :
+    m_1 = 4
+    n_1 = 5
+    board_1 = ["CCBDE", "AAADE", "AAABF", "CCBBF"]
+
+    m_2 = 6
+    n_2 = 6
+    board_2 = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]
+
+    print(solution(m_1, n_1, board_1))
+    # print(solution(m_2, n_2, board_2))
---
+++ 2018_KAKAO_RECRUIT/That_Song/17683.py
@@ -0,0 +1,17 @@
+def solution(m, musicinfos):
+    answer = ''
+    return answer
+
+if __name__ == '__main__' :
+    m_1 = "ABCDEFG"
+    musicinfos_1 = ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]
+
+    m_2 = "CC#BCC#BCC#BCC#B"
+    musicinfos_2 = ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]
+
+    m_3 = "ABC"
+    musicinfos_3 = ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]
+
+    print(solution(m_1, musicinfos_1))
+    # print(solution(m_2, musicinfos_2))
+    # print(solution(m_3, musicinfos_3))
