4e0f01ed676623ecd4ec47cf8513c1b66192535f
moonpiderman <lotsofchuck@gmail.com>
2021-07-12 13:08:52 +0900

    fin q

--- 2020_KAKAO_INTERN/Maximum_format/67257.py
+++ 2020_KAKAO_INTERN/Maximum_format/67257.py
@@ -4,11 +4,9 @@ def list_sort(opertator) :
     answer_list = [ 0 for i in range(len(t)) ]
     return t, answer_list
 
-def solution(expression):
+def solution_fail(expression):
     operator = []
     expression = list(expression)
-
-
 
     for val in expression :
         if not val.isnumeric() :
@@ -18,9 +16,36 @@ def solution(expression):
                 operator.append(val)
     # operator sort
     permu, answer_list = list_sort(operator)
-
-
     return max(answer_list)
+
+def calcul(operator, n, ex) :
+    if n == 2 :
+        return str(eval(ex))
+    if operator[n] == '+' :
+        result = eval('+'.join([calcul(operator, n+1, e) for e in ex.split('+')]))
+    if operator[n] == '-' :
+        result = eval('-'.join([calcul(operator, n+1, e) for e in ex.split('-')]))
+    if operator[n] == '*' :
+        result = eval('*'.join([calcul(operator, n+1, e) for e in ex.split('*')]))
+
+    return str(result)
+
+def solution(expression) :
+    answer = 0
+    operators = [
+        ('+', '-', '*'),
+        ('+', '*', '-'),
+        ('*', '-', '+'),
+        ('*', '+', '-'),
+        ('-', '*', '+'),
+        ('-', '+', '*')
+    ]
+    for oper in operators :
+        r = int(calcul(oper, 0, expression))
+        answer = max(answer, abs(r))
+
+    return answer
+
 
 if __name__ == '__main__' :
     expression_1 = "100-200*300-500+20"
