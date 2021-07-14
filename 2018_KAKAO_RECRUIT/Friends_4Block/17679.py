5f9be09f5e363df50f4cf415d9fb7b2104f06949
moonpiderman <lotsofchuck@gmail.com>
2021-07-14 21:26:18 +0900

    fuck that 4 blocks

--- 2018_KAKAO_RECRUIT/Friends_4Block/17679.py
+++ 2018_KAKAO_RECRUIT/Friends_4Block/17679.py
@@ -1,5 +1,36 @@
 def solution(m, n, board):
     answer = 0
+    col = n
+    row = m
+    # board 쪼개기
+    for i in range(len(board)) :
+        board[i] = list(board[i])
+
+    while True :
+        remove = [[0 for i in range(col)] for _ in range(row)]
+        for r in range(row - 1) :
+            for c in range(col - 1) :
+                if board[r][c] != 0 and board[r][c] == board[r][c + 1] and board[r][c] == board[r + 1][c] and board[r][c] == board[r + 1][c + 1] :
+                    remove[r][c], remove[r + 1][c], remove[r][c + 1], remove[r + 1][c + 1] = 1, 1, 1, 1
+
+        cnt = 0
+        for k in range(row) :
+            cnt += sum(remove[k])
+        answer += cnt
+        if cnt == 0 :
+            break
+        for i in range(row - 1, -1, -1) :
+            for j in range(col) :
+                if remove[i][j] == 1:
+                    x = i - 1
+                    while x >= 0 and remove[x][j] == 1:
+                        x -= 1
+                    if x < 0 :
+                        board[i][j] = 0
+                    else :
+                        board[i][j] = board[x][j]
+                        remove[x][j] = 1
+
     return answer
 
 if __name__ == '__main__' :
@@ -11,5 +42,5 @@ if __name__ == '__main__' :
     n_2 = 6
     board_2 = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]
 
-    print(solution(m_1, n_1, board_1))
-    # print(solution(m_2, n_2, board_2))
+    # print(solution(m_1, n_1, board_1))
+    print(solution(m_2, n_2, board_2))
