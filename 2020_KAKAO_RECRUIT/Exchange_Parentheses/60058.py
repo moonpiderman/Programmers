8bb101cdf0c52b1a3bd17a7608ce5dada5574c62
moonpiderman <lotsofchuck@gmail.com>
2021-07-15 22:03:33 +0900

    fuck

---
+++ 2020_KAKAO_RECRUIT/Exchange_Parentheses/60058.py
@@ -0,0 +1,20 @@
+def solution(p):
+    answer = ''
+    p = list(p)
+    if len(p) == 0 :
+        return answer
+
+
+
+    return answer
+
+if __name__ == '__main__' :
+    p_1 = "(()())()"
+    p_2 = ")("
+    p_3 = "()))((()"
+    p_4 = ""
+
+    print(solution(p_1))
+    # print(solution(p_2))
+    # print(solution(p_3))
+    # print(solution(p_4))
