8bce01ca6a8997e850d4095fed5a096a2dbfa93d
moonpiderman <lotsofchuck@gmail.com>
2021-07-17 12:04:38 +0900

    fin

--- 2018_KAKAO_RECRUIT/Compression/17684.py
+++ 2018_KAKAO_RECRUIT/Compression/17684.py
@@ -1,15 +1,5 @@
 def solution(msg):
     answer = []
-    msg = list(msg)
-
-    # dic = {
-    #     1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E',
-    #     6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J',
-    #     11: 'K', 12: 'L', 13: 'M', 14: 'N', 15: 'O', 16: 'P',
-    #     17: 'Q', 18: 'R', 19: 'S', 20: 'T', 21: 'U', 22: 'V',
-    #     23: 'W', 24: 'X', 25: 'Y', 26: 'Z'
-    # }
-
     dic_al = {
         'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6,
         'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12,
@@ -17,15 +7,19 @@ def solution(msg):
         'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24,
         'Y': 25, 'Z': 26
     }
-
-    # msg의 원소가 stack(?)에 있으면 그 다음을 추가해서 다시 answer(?) 탐색하기
-    for idx in range(len(msg)):
-        chk = msg[idx]
-        if dic_al[chk] not in answer:
-            answer.append(dic_al[chk])
-
-
-
+    dic_size = 26
+    buffer = ""
+    for al in msg:
+        test = buffer + al
+        if test in list(dic_al.keys()):
+            buffer = test
+        else:
+            answer.append(dic_al[buffer])
+            dic_size += 1
+            dic_al[test] = dic_size
+            buffer = al
+    if buffer:
+        answer.append(dic_al[buffer])
 
     return answer
 
@@ -35,5 +29,5 @@ if __name__ == '__main__':
     msg_3 = "ABABABABABABABAB"
 
     print(solution(msg_1))
-    # print(solution(msg_2))
-    # print(solution(msg_3))
+    print(solution(msg_2))
+    print(solution(msg_3))
