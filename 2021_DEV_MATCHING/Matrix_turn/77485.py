26e487d6b1c242d3e24422f65008d21e5cc550f7
moonpiderman <lotsofchuck@gmail.com>
2021-07-20 11:51:00 +0900

    matrix turn start

---
+++ 2021_DEV_MATCHING/Matrix_turn/77485.py
@@ -0,0 +1,21 @@
+def solution(rows, columns, queries):
+    answer = []
+
+    return answer
+
+if __name__ == '__main__':
+    rows_1 = 6
+    columns_1 = 6
+    queries_1 = [[2, 2, 5, 4], [3, 3, 6, 6], [5, 1, 6, 3]]
+
+    rows_2 = 3
+    columns_2 = 3
+    queries_2 = [[1, 1, 2, 2], [1, 2, 2, 3], [2, 1, 3, 2], [2, 2, 3, 3]]
+
+    rows_3 = 100
+    columns_3 = 97
+    queries_3 = [[1, 1, 100, 97]]
+
+    print(solution(rows_1, columns_1, queries_1))
+    # print(solution(rows_2, columns_2, queries_2))
+    # print(solution(rows_3, columns_3, queries_3))
