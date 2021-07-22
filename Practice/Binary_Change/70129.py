1303cf96bf99b819f5fbe9aee2314fe88d41de4d
moonpiderman <lotsofchuck@gmail.com>
2021-07-22 10:30:51 +0900

    fin

--- Practice/Binary_Change/70129.py
+++ Practice/Binary_Change/70129.py
@@ -1,32 +1,36 @@
-def counting(string):
-    total_c = 0
-    zero_c = 0
+def counting(string, total, zero):
+    total_c = total
+    zero_c = zero
     aws = string
     tmp = []
 
-    while aws != '1':
-        aws = list(aws)
-        for i in range(len(aws)):
-            if aws[i] == '0':
-                zero_c += 1
-            elif aws[i] == '1':
-                tmp.append(aws[i])
 
-        length = len(tmp)
-        binary = bin(length)
-        aws = binary[2:]
-        total_c += 1
+    aws = list(aws)
+    for i in range(len(aws)):
+        if aws[i] == '0':
+            zero_c += 1
+        elif aws[i] == '1':
+            tmp.append(aws[i])
 
-    return total_c, zero_c
+    length = len(tmp)
+    binary = bin(length)
+    aws = binary[2:]
+    total_c += 1
+
+    return aws, total_c, zero_c
 
 def solution(s):
     answer = []
-    total_cnt, zero_cnt = counting(s)
-    print(total_cnt)
-    print(zero_cnt)
+    total_cnt = 0
+    zero_cnt = 0
 
-    # while s != '1':
-    #     s, total_cnt, zero_cnt = counting(s, total_cnt, zero_cnt)
+    while True:
+        s, total_cnt, zero_cnt = counting(s, total_cnt, zero_cnt)
+        if s == '1':
+            break
+
+    answer.append(total_cnt)
+    answer.append(zero_cnt)
 
     return answer
 
@@ -36,5 +40,5 @@ if __name__ == '__main__':
     s_3 = "1111111"
 
     print(solution(s_1))
-    # print(solution(s_2))
-    # print(solution(s_3))
+    print(solution(s_2))
+    print(solution(s_3))
