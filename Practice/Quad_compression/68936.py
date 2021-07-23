c2be636647ccf368c9fd2cda2813572370b6ad63
moonpiderman <lotsofchuck@gmail.com>
2021-07-23 16:07:45 +0900

    my code is fail

--- Practice/Quad_compression/68936.py
+++ Practice/Quad_compression/68936.py
@@ -1,9 +1,31 @@
+def quater(array, r_s, r_e, c_s, c_e):
+    quat = []
+    half = len(array) // 2
+    all = len(array)
+    for i in range(r_s, r_e):
+        tmp = []
+        for j in range(c_s, c_e):
+            tmp.append(array[i][j])
+        quat.append(tmp)
+    return quat
+
+def solution_fail(arr):
+    answer = []
+    distance = len(arr)
+
+    while distance > 2:
+        half = distance // 2
+        first_quat = quater(arr, 0, half, 0, half)
+        second_quat = quater(arr, 0, half, half, distance)
+        third_quat = quater(arr, half, distance, 0, half)
+        fourth_quat = quater(arr, half, distance, half, distance)
+
+        # 잘라낸 내부에서 0, 1 체크?
+        distance = half
+    return answer
+
 def solution(arr):
     answer = []
-    max_quad = len(arr)
-
-    
-
     return answer
 
 if __name__ == '__main__':
@@ -24,5 +46,5 @@ if __name__ == '__main__':
         [0, 0, 0, 0, 1, 1, 1, 1]
     ]
 
-    print(solution(arr_1))
-    # print(solution(arr_2))
+    # print(solution(arr_1))
+    print(solution(arr_2))
