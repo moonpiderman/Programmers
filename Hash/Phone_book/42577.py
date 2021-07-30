69768eb570b4f03fc584ef80fa338cab6998e578
moonpiderman <lotsofchuck@gmail.com>
2021-07-30 20:16:32 +0900

    edit later

--- Hash/Phone_book/42577.py
+++ Hash/Phone_book/42577.py
@@ -10,9 +10,7 @@ def solution(phone_book):
 
 def solution_later(phone_book):
     phone_book = sorted(phone_book, key=len)
-
-    for i in range(len(phone_book)):
-        for j in range(i + 1, len(phone_book)):
-            if phone_book[j].startswith(phone_book[i]):
-                return False
+    for i in range(len(phone_book) - 1):
+        if phone_book[i + 1].startswith(phone_book[i]):
+            return False
     return True
