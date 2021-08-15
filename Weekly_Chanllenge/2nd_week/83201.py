bb74b4d1cf235c76f93c8379e5382d0017ad2465
moonpiderman <lotsofchuck@gmail.com>
2021-08-15 19:33:30 +0900

    start week2

---
+++ Weekly_Chanllenge/2nd_week/83201.py
@@ -0,0 +1,27 @@
+def solution(scores):
+    answer = ''
+    return answer
+
+if __name__ == '__main__':
+    scores_1 = [
+        [100, 90, 98, 88, 65],
+        [50, 45, 99, 85, 77],
+        [47, 88, 95, 80, 67],
+        [61, 57, 100, 80, 65],
+        [24, 90, 94, 75, 65]
+    ]
+
+    scores_2 = [
+        [50, 90],
+        [50, 87]
+    ]
+
+    scores_3 = [
+        [70, 49, 90],
+        [68, 50, 38],
+        [73, 31, 100]
+    ]
+
+    print(solution(scores_1))
+    # print(solution(scores_2))
+    # print(solution(scores_3))
