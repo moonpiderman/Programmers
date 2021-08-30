fe78156677beec52298bb30d85e63df9b3f081ec
moonpiderman <lotsofchuck@gmail.com>
2021-08-30 14:24:50 +0900

    fin 4th_week

--- Weekly_Chanllenge/4th_week/84325.py
+++ Weekly_Chanllenge/4th_week/84325.py
@@ -1,6 +1,31 @@
 def solution(table, languages, preference):
-    answer = ''
-    return answer
+    answer = []
+    dic = {}
+    pre = {}
+    maxi = 0
+
+    for i in range(len(languages)):
+        pre[languages[i]] = preference[i]
+
+    for i in range(len(table)):
+        li = table[i].split()
+        table[i] = li
+        dic[li[0]] = 0
+
+    for i in range(len(table)):
+        for j in range(1, 6):
+           if table[i][j] in pre.keys():
+               dic[table[i][0]] += pre[table[i][j]] * (6 - j)
+
+    s_dict = sorted(dic.items(), key=lambda x:x[1], reverse=True)
+    answer.append(s_dict[0][0])
+    for i in range(1, len(s_dict)):
+        if s_dict[0][1] == s_dict[i][1]:
+            answer.append(s_dict[i][0])
+
+    answer.sort()
+
+    return answer[0]
 
 if __name__ == '__main__':
     table_1 = [
@@ -27,5 +52,5 @@ if __name__ == '__main__':
 
     preference_2 = [7, 5]
 
-    print(solution(table_1, languages_1, preference_1))
-    # print(solution(table_2, languages_2, preference_2))
+    # print(solution(table_1, languages_1, preference_1))
+    print(solution(table_2, languages_2, preference_2))
