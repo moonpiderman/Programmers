afe1c41f6ff16a3344fbe22793fab96133d41993
moonpiderman <lotsofchuck@gmail.com>
2021-08-30 15:05:15 +0900

    start 5th_week

---
+++ Weekly_Chanllenge/5th_week/84512.py
@@ -0,0 +1,14 @@
+def solution(word):
+    answer = 0
+    return answer
+
+if __name__ == '__main__':
+    word_1 = "AAAAE"
+    word_2 = "AAAE"
+    word_3 = "I"
+    word_4 = "EIO"
+
+    print(solution(word_1))
+    # print(solution(word_2))
+    # print(solution(word_3))
+    # print(solution(word_4))
