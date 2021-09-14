2951f808265362b3f58aa89469be0ef4a96c07f0
moonpiderman <lotsofchuck@gmail.com>
2021-09-14 13:32:29 +0900

    set

---
+++ Weekly_Chanllenge/6th_week/85002.py
@@ -0,0 +1,17 @@
+def solution(weights, head2head):
+    answer = []
+    return answer
+
+if __name__ == '__main__':
+    weights_1 = [50, 82, 75, 120]
+    head2head_1 = ["NLWL", "WNLL", "LWNW", "WWLN"]
+
+    weights_2 = [145, 92, 86]
+    head2head_2 = ["NLW", "WNL", "LWN"]
+
+    weights_3 = [60, 70, 60]
+    head2head_3 = ["NNN", "NNN", "NNN"]
+
+    print(solution(weights_1, head2head_1))
+    # print(solution(weights_2, head2head_2))
+    # print(solution(weights_3, head2head_3))
