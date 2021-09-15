38f52b8503453fb5768c5d71c5fdf1ea2990618c
moonpiderman <lotsofchuck@gmail.com>
2021-09-15 16:22:22 +0900

    solve

--- Weekly_Chanllenge/6th_week/85002.py
+++ Weekly_Chanllenge/6th_week/85002.py
@@ -1,5 +1,30 @@
 def solution(weights, head2head):
-    answer = []
+    field = []
+
+    # winning rate
+    for i in range(len(weights)):
+        fight_cnt = 0
+        w_cnt = 0
+        heavy_cnt = 0
+        for j in range(len(weights)):
+            if head2head[i][j] != 'N':
+                fight_cnt += 1
+                if head2head[i][j] == 'W':
+                    w_cnt += 1
+                    if weights[i] < weights[j]:
+                        heavy_cnt += 1
+        if fight_cnt == 0:
+            w_rate = 0
+        else:
+            w_rate = w_cnt / fight_cnt
+        # i_player = ( i + 1, weight[i], winning_rate, up_cnt )
+        i_player = (i + 1, weights[i], w_rate, heavy_cnt)
+        field.append(i_player)
+
+    sorted_field = sorted(field, key=lambda x: (-x[2], -x[3], -x[1], x[0]))
+
+    answer = [x[0] for x in sorted_field]
+
     return answer
 
 if __name__ == '__main__':
@@ -13,5 +38,5 @@ if __name__ == '__main__':
     head2head_3 = ["NNN", "NNN", "NNN"]
 
     print(solution(weights_1, head2head_1))
-    # print(solution(weights_2, head2head_2))
-    # print(solution(weights_3, head2head_3))
+    print(solution(weights_2, head2head_2))
+    print(solution(weights_3, head2head_3))
