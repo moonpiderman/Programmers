c18bc54ff144a339ea0f25989a14bec9fad22243
moonpiderman <lotsofchuck@gmail.com>
2021-12-25 09:19:43 +0900

    algorithm restart

---
+++ Weekly_Chanllenge/7th_week/86048.py
@@ -0,0 +1,25 @@
+def solution(enter, leave):
+    answer = []
+    return answer
+
+if __name__ == '__main__':
+    enter_1 = [1, 3, 2]
+    leave_1 = [1, 2, 3]
+
+    enter_2 = [1, 4, 2, 3]
+    leave_2 = [2, 1, 3, 4]
+
+    enter_3 = [3, 2, 1]
+    leave_3 = [2, 1, 3]
+
+    enter_4 = [3, 2, 1]
+    leave_4 = [1, 3, 2]
+
+    enter_5 = [1, 4, 2, 3]
+    leave_5 = [2, 1, 4, 3]
+
+    print(solution(enter_1, leave_1))
+    print(solution(enter_2, leave_2))
+    print(solution(enter_3, leave_3))
+    print(solution(enter_4, leave_4))
+    print(solution(enter_5, leave_5))
